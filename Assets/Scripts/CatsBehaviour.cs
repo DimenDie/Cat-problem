884ae96ba7fb6432d9a614ffb2c8d513f6259a90
DimenDie <dimarepalo3@gmail.com>
2022-07-15 17:38:32 +0300

    Update CatsBehaviour.cs
    A lot of things need to be fixed

--- Assets/Scripts/CatsBehaviour.cs
+++ Assets/Scripts/CatsBehaviour.cs
@@ -7,8 +7,8 @@ public class CatsBehaviour : MonoBehaviour
     [SerializeField] private GameObject cat1;
     [SerializeField] private GameObject cat2;
     public GameObject[] places = new GameObject[7];
-    private int cat1Counter;
-    private int cat2Counter;
+    [SerializeField] private int cat1Counter;
+    [SerializeField] private int cat2Counter;
     
     // Start is called before the first frame update
     void Start()
@@ -22,19 +22,23 @@ public class CatsBehaviour : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log(cat1Counter + ":" + cat2Counter);
-
-            if (cat1Counter + 1 == cat2Counter)
-                cat1Counter++;
-            cat1.transform.position = places[cat1Counter].transform.position + Vector3.up / 2;
-            cat1Counter++;
-            cat1Counter = cat1Counter % places.Length;
-
-
             cat2.transform.position = places[cat2Counter].transform.position + Vector3.up / 2;
+            cat1.transform.position = places[cat1Counter].transform.position + Vector3.up / 2;
+
+            cat2Counter = cat2Counter / places.Length;
             cat2Counter--;
-            if (cat2Counter == -1)
-                cat2Counter = places.Length - 1;
+
+
+            cat1Counter = cat1Counter % (places.Length - 1);
+
+            cat1Counter++;
+            if (cat1Counter == cat2Counter)
+            {
+                cat1Counter++;
+            }
+
+
+            Debug.Log(cat1Counter + ":" + cat2Counter);
 
         }
     }
